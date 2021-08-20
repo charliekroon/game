1c396f6f0e2daccd3905bfa3270a8d3bcc1dcbc7
charlie <charlie@hackerone.com>
2021-08-20 16:42:29 +0200

    Refactoring

--- studio_game.rb
+++ studio_game.rb
@@ -6,9 +6,9 @@ player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
 knuckleheads = Game.new("Knuckleheads")
-knuckleheads.add_player(player1)
-knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player1)
 knuckleheads.play(3)
 knuckleheads.print_stats
 
